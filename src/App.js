aaa39f10b5269ab4f7481b8a339fd777fee23b56
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-10-03 23:25:28 +0300

    Combine Image, Title & Author compnents into a single Book component

--- src/App.js
+++ src/App.js
@@ -19,18 +19,11 @@ function App() {
 const Book = () => {
   return (
       <article className='book'>
-        <Image/>
-        <Title/>
-        <Author/>
+        <img src="https://images-na.ssl-images-amazon.com/images/I/81l7K528xDL._AC_UL320_SR208,320_.jpg" alt=""/>
+        <h1>Cracking the Tech Career: Insider Advice on Landing a Job at Google, Microsoft, Apple, or any Top Tech Company</h1>
+        <h4>Gayle Laakmann McDowell</h4>
       </article>
   )
 }
-
-const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/I/81l7K528xDL._AC_UL320_SR208,320_.jpg" alt=""/>
-
-const Title = () => <h1>Cracking the Tech Career: Insider Advice on Landing a Job at Google, Microsoft, Apple, or any Top Tech Company 
-</h1>
-
-const Author = () => <h4 style={{color: '#617d98', fontSize: '0.98rem', marginTop:'0.5rem'}}>Gayle Laakmann McDowell</h4>
 
 export default App;
