8994e5ccbb2ac9cb98c183c1f5f3fc7512e2f568
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-10-04 21:45:17 +0300

    Add props to the Book component & style the Book component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,79 @@
 import React from 'react';
+import './app.css';
+
+// Books
+const firstBook = {
+  img: 'https://images-na.ssl-images-amazon.com/images/I/81l7K528xDL._AC_UL320_SR208,320_.jpg',
+  title: 'Cracking the Tech Career: Insider Advice on Landing a Job at Google, Microsoft, Apple, or any Top Tech Company',
+  author: 'Gayle Laakmann McDowell'
+}
+
+const secondBook = {
+  img: 'https://images-na.ssl-images-amazon.com/images/I/410hiaPGyCL._SX348_BO1,204,203,200_.jpg',
+  title: 'Cracking the Coding Interview: 189 Programming Questions and Solutions 6th Edition',
+  author: 'Gayle Laakmann McDowell'
+}
+
 
 // CSS
-import './app.css';
 
 function App() {
   return (
     <section className="bookList">
-      <Book/>
-      <Book/>
-      <Book/>
-      <Book/>
-      <Book/>
-      <Book/>
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 }
 
-const Book = () => {
+const Book = ({img, title, author}) => {
   return (
       <article className='book'>
-        <img src="https://images-na.ssl-images-amazon.com/images/I/81l7K528xDL._AC_UL320_SR208,320_.jpg" alt=""/>
-        <h1>Cracking the Tech Career: Insider Advice on Landing a Job at Google, Microsoft, Apple, or any Top Tech Company</h1>
-        <h4>Gayle Laakmann McDowell</h4>
+        <img src={img} alt=""/>
+        <h1>{title}</h1>
+        <h4>{author}</h4>
       </article>
   )
 }
