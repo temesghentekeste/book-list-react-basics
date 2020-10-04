fb09099ac1684dcd935f638fe2784b91b29b19f1
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-10-04 23:55:20 +0300

    Event basics

--- src/App.js
+++ src/App.js
@@ -64,11 +64,22 @@ function App() {
 
 const Book = (props) => {
   const {img, title, author} = props;
+
+  const clickHandler = (e) => {
+    console.log(e);
+    console.log(e.target);
+  }
+
+  const clickHandlerComplex = author => alert(author);
+
+  const handleMouseOver = ()=>console.log(author);
   return (
-      <article className='book'>
+      <article className='book' onMouseOver={handleMouseOver}>
         <img src={img} alt=""/>
-        <h1>{title}</h1>
+        <h1 onClick= {()=>console.log(title)}>{title}</h1>
         <h4>{author}</h4>
+        <button type='button' onClick={clickHandler}>Refernce example</button>
+        <button type='button' onClick={()=>clickHandlerComplex(author)}>Complex Event Handler</button>
       </article>
   )
 }
